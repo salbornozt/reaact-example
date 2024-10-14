6fff5034c28e283de96239648edce88d0b8ef998
salbornozt <salbornozt@unbosque.edu.co>
2024-10-14 11:31:17 -0500

    first commit

---
+++ app/about.tsx
@@ -0,0 +1,23 @@
+import { View, StyleSheet, Text } from "react-native";
+
+export default function AboutScreen(){
+    return (
+        <View style = {styles.container}>
+            <Text style = {styles.text} >About Screen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create(
+    {
+        container : {
+            flex : 1,
+            backgroundColor: '#25292e',
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        text : {
+            color: '#fff',
+        }
+    }
+);
